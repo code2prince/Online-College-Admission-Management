f1ec985d4810eb9260b45861701a79603ef1e8fc
Rajeev Sharma <talk2rajeev1986@gmail.com>
2022-06-07 18:38:51 +0530

    nodemon npm added, GET POST PUT DELETE apii example created

--- server.js
+++ server.js
@@ -3,8 +3,69 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, 'client')))
 .get('/', (request, response) => response.render('index.html'));
+
+/***
+ * GET >> to get the data from server
+ * POST >> to send data to server (generally when we submit any form)
+ * DELETE >> to send data to server (generally to delete any record)
+ * PUT >> to send data to server (generally to update the record)
+ * PATCH >> to send data to server (generally to do minor updates)
+ * /
+*/
+
+const studentList = [
+    {id: 1, name: 'prince'},
+    {id: 2, name: 'Rajeev'},
+];
+
+app.get('/getStudetList', function(request, response){
+    // resquest > which is coming from client, it coantain all the info related to clinet
+    // response > which is sent to client
+
+
+    //send data to client
+    response.send(studentList);
+});
+
+app.get('/getStudetList/:id', function(request, response){
+    // resquest > which is coming from client, it coantain all the info related to clinet
+    // response > which is sent to client
+
+    console.log('print request params >>>>>>> ', request.params);
+    const id = parseInt(request.params.id);
+    const foundStudent = studentList.find((x) => x.id === id)
+
+    if(foundStudent) {     
+        //send data to client
+        response.send(foundStudent);
+    }
+    else {
+        response.send('student not found');
+    }
+});
+
+app.post('/enrollStudent', function(request, response){
+    //var formData = request.body;
+    console.log('req body from POST call ================ ', request.body);
+    response.send({msg: 'data saved to server'});
+});
+
+app.put('/putExample', function(request, response){
+    //var formData = request.body;
+    console.log('req body from POST call ================ ', request.body);
+    response.send(request.body);
+});
+
+
+app.delete('/deleteExample', function(request, response){
+    //var formData = request.body;
+    console.log('req body from POST call ================ ', request.body);
+    response.send(request.body);
+});
 
 
 
@@ -12,3 +73,6 @@ app.use(express.static(path.join(__dirname, 'client')))
 app.listen(3000, function() {
     console.log('server is running on port 3000');
 });
+
+
+
